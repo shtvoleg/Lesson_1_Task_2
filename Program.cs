706da18c361fc1b20868edb17962be31dae3f14c
shtv_oleg <ov09122014@mail.ru>
2023-01-27 13:43:51 +0400

    Lesson_1_Task_2

--- Program.cs
+++ Program.cs
@@ -8,7 +8,10 @@ int n1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите 2-е число: ");		// ввод 2-го числа
 int n2 = Convert.ToInt32(Console.ReadLine());
 
-if (n1 > n2)
-    Console.WriteLine($"Максимум: {n1}.");
-else    
-    Console.WriteLine($"Максимум: {n2}.");
+if (n1 == n2)
+    Console.WriteLine($"Числа равны: {n1}={n2}.");
+else
+    if (n1 > n2)
+        Console.WriteLine($"Большее число: {n1}. Меньшее число: {n2}.");
+    else    
+        Console.WriteLine($"Большее число: {n2}. Меньшее число: {n1}.");
